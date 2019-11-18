586e79441a48e934559fa3743d0e4c54d1bfcb71
resiuji <44506672+resiuji@users.noreply.github.com>
2019-11-19 00:12:10 +0700

    Tugas Ajax

--- js/tes.js
+++ js/tes.js
@@ -85,3 +85,11 @@ function validateForm() {
     }
     return isValid;
 }
+
+$('#manual-ajax').click(function(event) {
+event.preventDefault();
+this.blur(); // Manually remove focus from clicked link.
+$.get(this.href, function(html) {
+  $(html).appendTo('body').modal();
+});
+});
