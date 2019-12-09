40cf090dbb67838a491ce261f669700c07d39f97
resiuji <44506672+resiuji@users.noreply.github.com>
2019-12-10 01:21:03 +0700

    Pemindahan file + Modal
    Pemindahan gambar dan css ke folder yg berbeda. Penambahan modal pada halaman Pemesanan2

--- js/tes.js
+++ js/tes.js
@@ -86,10 +86,24 @@ function validateForm() {
     return isValid;
 }
 
-$('#manual-ajax').click(function(event) {
-event.preventDefault();
-this.blur(); // Manually remove focus from clicked link.
-$.get(this.href, function(html) {
-  $(html).appendTo('body').modal();
-});
-});
+function MyModal() {
+  var modal = document.getElementById("myModal");
+
+  var btn = document.getElementById("myBtn");
+
+  var span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
